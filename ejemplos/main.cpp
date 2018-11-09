4afd18d232b421e50af0badace860fe1ab1795d1
david <dchoqueluque@gmail.com>
2018-11-09 14:19:19 -0500

    avance

--- ejemplos/main.cpp
+++ ejemplos/main.cpp
@@ -1,25 +1,32 @@
-#include <GL/glut.h>
- 
-void displayMe(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
-        glVertex3f(0.5, 0.0, 0.5);
-        glVertex3f(0.5, 0.0, 0.0);
-        glVertex3f(0.0, 0.5, 0.0);
-        glVertex3f(0.0, 0.0, 0.5);
-    glEnd();
-    glFlush();
-}
- 
-int main(int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(400, 300);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Hello world!");
-    glutDisplayFunc(displayMe);
-    glutMainLoop();
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+//g++ -o hellot main.cpp -lglut -lglfw -lGL -lGLU -lGLEW
+
+#include <stdio.h>
+
+int main(){
+    if(!glfwInit()){
+        fprintf(stderr,"ERROR: no se puede iniciar GLFW3\n");
+        return 1;
+    }
+    GLFWwindow* window = glfwCreateWindow(640,480,"hola triangulo",NULL,NULL);
+    if(!window){
+        fprintf(stderr,"ERROR: No se puede abrir window\n");
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    //version info
+    const GLubyte* renderer = glGetString(GL_RENDERER);
+    const GLubyte* version = glGetString(GL_VERSION);
+    printf("Renderer: %s\n",renderer);
+    printf("OpenGL version soportada %s\n",version);
+    glfwTerminate();
     return 0;
+    
 }
